ad14d10132e372a14f5274fadcf15016dd3b0cde
imbroiniselucas-cmyk <imbroinise.lucas@gmail.com>
2026-02-05 08:03:22 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os, time
 token = (os.getenv("TELEGRAM_TOKEN") or "").strip()
 print("TOKEN_EXISTS:", bool(token))
 print("TOKEN_HAS_COLON:", ":" in token)
-print("TOKEN_LEN:", len(token))
+print("TOKEN_LEN:", len(token)) 
 
 while True:
     time.sleep(60)
